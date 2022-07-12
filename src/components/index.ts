916907d83f7f02a0741897e4787daf5a1905b793
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2022-07-12 12:31:26 -0300

    remove unnecessary import and add an export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Navbar } from './Navbar';
 export { default as Product } from './Product';
+export { default as ShoppingCart } from './ShoppingCart';
+export { default as CartItem } from './CartItem';
